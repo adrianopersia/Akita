3cb37de4a21b39dad8c0349f59dccde5a31afd6d
adriano persia <adrianopersia@gmail.com>
2023-07-03 09:07:58 -0300

    added functional product and user js, and modified some html files in product

--- scripts/product_scripts.js
+++ scripts/product_scripts.js
@@ -1,6 +1,28 @@
+document.getElementById("popup").style.display = "none";
+document.getElementById("image").addEventListener("click", function(){
+    let popupMenu = document.getElementById("popup");
+    if (popupMenu.style.display === "none"){
+      popupMenu.style.display = "block";
+      popupMenu.animate([{opacity:'0.0'}, {opacity:'1.0'}],
+        {duration: 300, fill:'forwards'})
+    }else{
+      popupMenu.style.display = "none";
+    }
+});
+
 document.getElementById("popupproduct").style.display = "none";
-const productPopup = document.getElementById("popupproduct");
+document.getElementById("cat").addEventListener("click", function(){
+    let popupProduct = document.getElementById("popupproduct");
+    if (popupProduct.style.display === "none"){
+      popupProduct.style.display = "block";
+      popupProduct.animate([{opacity:'0.0'}, {opacity:'1.0'}],
+        {duration: 300, fill:'forwards'})
+    }else{
+      popupProduct.style.display = "none";
+    }
+});
 
 document.getElementById("buttonPopup").addEventListener("click", function() {
-    productPopup.style.display = "none";
+    let popupProduct = document.getElementById("popupproduct");
+    popupProduct.style.display = "none";
 });
--- scripts/user_scripts.js
+++ scripts/user_scripts.js
@@ -10,3 +10,20 @@ registerLink.addEventListener("click", function() {
 loginLink.addEventListener("click", function() {
   wrapper.classList.remove("active");
 });
+
+document.getElementById("popup").style.display = "none";
+document.getElementById("image").addEventListener("click", function(){
+    let popupMenu = document.getElementById("popup");
+    if (popupMenu.style.display === "none"){
+      popupMenu.style.display = "block";
+      popupMenu.animate([{opacity:'0.0'}, {opacity:'1.0'}],
+        {duration: 300, fill:'forwards'})
+    }else{
+      popupMenu.style.display = "none";
+    }
+});
+
+document.getElementById("buttonPopup").addEventListener("click", function() {
+    let popupMenu = document.getElementById("popup");
+    popupMenu.style.display = "none";
+});
